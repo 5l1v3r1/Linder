83503c245d85162d9da77fd9744f0576882eec22
R37r0-Gh057 <40432896+R37r0-Gh057@users.noreply.github.com>
2019-09-04 23:06:33 +0530

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -2,7 +2,19 @@
 
 apt update && apt install python wget
 
-cd $HOME
-wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
-chmod +x setup.sh
-sh setup.sh
+if [ -e $PREFIX/bin/apkmod ]
+then
+  echo ALREADY INSTALLED
+else
+  cd $HOME
+  wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
+  chmod +x setup.sh
+  sh setup.sh
+  if [ -e $PREFIX/bin/apkmod ]
+  then
+    clear
+    echo ALL SET. EVERYTHING HAS INSTALLED.
+  else
+    echo The installation waws interrupted. please run this script again
+  fi
+fi
